e231882e73c6c3c10237611237369add94867c9b
Michael Zhou <micha.zhou@gmail.com>
2022-09-01 14:17:41 -0700

    added commit and run commands for c++ files on fish

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -5,4 +5,34 @@ function fish_user_key_bindings
     end
 
     alias rm="rm -i"
+
+    function compile -d "Compiles a c++ file with c++17 and all warnings" -a "path"
+        set index 1
+        set last_index (string length $argv[1])
+        for char in (string split '' $argv[1])
+            if [ $char != '.' ]
+                set index (math $index + 1)
+            else
+                set last_index $index
+            end
+        end
+        set out (string sub -s 1 -l (math $last_index - 1) $argv[1])
+
+        g++ -std=c++17 -O2 -o "$out" $1 -Wall $argv[1]
+    end
+
+    function run -d "Compiles and runs a c++ file" -a "path"
+        set index 1
+        set last_index (string length $argv[1])
+        for char in (string split '' $argv[1])
+            if [ $char != '.' ]
+                set index (math $index + 1)
+            else
+                set last_index $index
+            end
+        end
+        set out (string sub -s 1 -l (math $last_index - 1) $argv[1])
+
+        co $argv[1] && "./$out" & fg
+    end
 end
