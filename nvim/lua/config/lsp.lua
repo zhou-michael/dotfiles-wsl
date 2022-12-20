aec2cb7ddeedffd216a6cf1097bf26a1ebc102a8
Michael Zhou <micha.zhou@gmail.com>
2022-12-20 14:16:22 -0800

    migration to lualine, mason has automatic lsp setup, starship

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -1,26 +1,2 @@
-local lspconfig = require('lspconfig')
-local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
-
-local default_opt = {
-    capabilities = capabilities
-}
-
-lspconfig.sumneko_lua.setup({
-    settings = {
-        Lua = {
-            diagnostics = {
-                globals = {
-                    'vim',
-                    'use'
-                }
-            }
-        }
-    },
-    capabilities = capabilities
-})
-
-lspconfig.vimls.setup(default_opt)
-lspconfig.clangd.setup(default_opt)
-lspconfig.pyright.setup(default_opt)
-lspconfig.rust_analyzer.setup(default_opt)
+require('lspconfig.ui.windows').default_options.border = 'rounded'
 
